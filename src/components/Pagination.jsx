5bde1693634e44a056271876e57268dceb3045c1
Adityasusrname <adityasmailbooxx@gmail.com>
2023-02-07 23:54:19 +0530

    Pagination is working

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Pagination() {
+  let [pageNum,setPage]=useState(1)
+  const onPrev=()=>{
+        if(pageNum>1){
+          setPage(pageNum-1);
+        }
+  }
+  const onNext=()=>{
+    
+      setPage(pageNum+1);
+   
+}
   return (
     <div className='flex justify-center my-4'>
-        <div className='border-2 p-2 border-r-0 rounded-l-xl border-blue-400'>Previous</div>
-        <div className='border-2 p-2 border-r-0 border-blue-400'>1</div>
-        <div className='border-2 p-2 rounded-r-xl border-blue-400'>Next</div>
+        <div className='border-2 p-2 border-r-0 rounded-l-xl border-blue-400' onClick={onPrev}>Previous</div>
+        <div className='border-2 p-2 border-r-0 border-blue-400'>{pageNum}</div>
+        <div className='border-2 p-2 rounded-r-xl border-blue-400' onClick={onNext}>Next</div>
     </div>
   )
 }
