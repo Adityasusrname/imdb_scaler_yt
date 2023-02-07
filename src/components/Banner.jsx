63b770390625944686bbbc8cc2581ddc05ce1d98
Adityasusrname <adityasmailbooxx@gmail.com>
2023-02-07 13:52:50 +0530

    Responsiveness done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,9 +6,9 @@ function Banner() {
   return (
     <>
     
-       <div className="bg-banner h-[40vh] bg-center bg-cover flex items-end ">
+       <div className="bg-banner h-[40vh] md:h-[60vh] bg-center bg-cover flex items-end ">
 
-        <div className='text-xl text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold'>
+        <div className='text-xl md:text-3xl text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold'>
             M3GAN
         </div>
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -14,7 +14,39 @@ function Movies() {
         flex-wrap
         justify-center'>
 
-          <div className="w-[150px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 
+          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
+          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
+           bg-center bg-cover flex items-end">
+
+            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
+
+          </div>
+
+          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
+          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
+           bg-center bg-cover flex items-end">
+
+            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
+
+          </div>
+
+          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
+          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
+           bg-center bg-cover flex items-end">
+
+            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
+
+          </div>
+
+          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
+          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
+           bg-center bg-cover flex items-end">
+
+            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
+
+          </div>
+
+          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
           bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
            bg-center bg-cover flex items-end">
 
