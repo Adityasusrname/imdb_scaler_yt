0a8f28d92207aaad28b4650022b153b3108c7209
Adityasusrname <adityasmailbooxx@gmail.com>
2023-02-07 13:34:24 +0530

    Movie card improved

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,14 +4,24 @@ import Image from '../banner.jpg'
 function Movies() {
   return (
     <div className='mt-8'>
+
         <div className='mb-8
         font-bold
         text-2xl
         text-center'>Trending Movies</div>
+
         <div className='flex
         flex-wrap
         justify-center'>
-            <img src={Image} className="w-[150px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300"/>
+
+          <div className="w-[150px] h-[30vh] m-4 rounded-xl hover:scale-110 duration-300 border-4 
+          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
+           bg-center bg-cover flex items-end">
+
+            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
+
+          </div>
+            
         </div>
     </div>
   )
