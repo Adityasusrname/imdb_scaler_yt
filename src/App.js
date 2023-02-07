2c602e0d9ca40c0287a0bce3a31c0ea7b77e9979
Adityasusrname <adityasmailbooxx@gmail.com>
2023-02-07 23:35:53 +0530

    Routing done

--- src/App.js
+++ src/App.js
@@ -4,28 +4,46 @@ import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Movies from './components/Movies';
 import Pagination from './components/Pagination';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Favourites from './components/Favourites';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
     <>
-   
+
+    <BrowserRouter>
+
     <NavBar></NavBar>
-    <Banner></Banner>
+
+    <Routes>
+   
+   <Route path="/" element={
+    <>
+      <Banner></Banner>
     <Movies></Movies>
     <Pagination></Pagination>
+    </>
+   }></Route>
 
-    {
+   <Route path="/fav" element={
 
-        /*
-             Nav Bar
-             Banner
-             Movies
-             Pagination
+    <Favourites/>
+
+   }></Route>
+
+   <Route path="*" element={<PageNotFound/>}></Route>
+    
+    </Routes>
+     
+   
+  
+
+    </BrowserRouter>
+   
+  
 
 
-        */
-
-    }
     </>
    
   );
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div>You have entered a wrong page</div>
+  )
+}
+
+export default PageNotFound
