dbe8d21e57fe4c6213284c78dae609be621197bc
Adityasusrname <adityasmailbooxx@gmail.com>
2023-02-08 20:15:06 +0530

    API connected

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Image from '../banner.jpg'
-
+import axios from "axios"
 
 function Banner() {
+
+    let [bannerMovie,setBanner] = useState("");
+   
+  useEffect(function(){
+
+    (function(){
+        axios.
+        get
+        ("https://api.themoviedb.org/3/trending/all/week?api_key=09df8f20c2dbf1dce6dd61c0a06c350c&page=3")
+        .then((res)=>{
+          console.table(res.data.results);
+          setBanner(res.data.results[0]);
+        })
+    })()
+
+  },[])
   return (
     <>
-    
-       <div className="bg-banner h-[40vh] md:h-[60vh] bg-center bg-cover flex items-end ">
+
+    {
+        bannerMovie == ""?<h1>Loading...</h1>: <div className={`bg-[url(https://image.tmdb.org/t/p/original/${bannerMovie.backdrop_path})] h-[40vh] md:h-[60vh] bg-center bg-cover flex items-end`}>
 
         <div className='text-xl md:text-3xl text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold'>
-            M3GAN
+            {bannerMovie.title}
         </div>
 
        </div>
+    }
+    
+      
       
     </>
   )
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Image from '../banner.jpg'
+import axios from "axios"
 
 function Movies() {
+  
   return (
     <div className='mt-8'>
 
@@ -20,41 +21,8 @@ function Movies() {
 
             <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
 
-          </div>
-
-          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
-          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
-           bg-center bg-cover flex items-end">
-
-            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
-
-          </div>
-
-          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
-          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
-           bg-center bg-cover flex items-end">
-
-            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
-
-          </div>
-
-          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
-          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
-           bg-center bg-cover flex items-end">
-
-            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
-
-          </div>
-
-          <div className="w-[150px] h-[30vh] md:h-[40vh] md:w-[180px] m-4 rounded-xl hover:scale-110 duration-300 border-4 
-          bg-[url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/dlrWhn0G3AtxYUx2D9P2bmzcsvF.jpg)]
-           bg-center bg-cover flex items-end">
-
-            <div className='font-bold text-white bg-gray-900 p-4 bg-opacity-40 text-center w-full font-bold rounded-b-xl'>M3GAN</div>
-
-          </div>
-            
-        </div>
+          </div>  
+            </div>
     </div>
   )
 }
